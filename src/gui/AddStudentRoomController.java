7be74338d7972072844ada0986bb35d5f83f2229
MilanVissers <milan.vissers@ugent.be>
2021-12-08 17:20:14 +0100

    files

--- src/gui/AddStudentRoomController.java
+++ src/gui/AddStudentRoomController.java
@@ -8,7 +8,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import logic.*;
 
