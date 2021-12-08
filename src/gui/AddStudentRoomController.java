de9c94ad8f57997224bf6e7fce537f359fcb6d6c
MilanVissers <milan.vissers@ugent.be>
2021-12-08 18:11:26 +0100

    files

--- src/gui/AddStudentRoomController.java
+++ src/gui/AddStudentRoomController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import database.DBBuilding;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -146,4 +147,45 @@ public class AddStudentRoomController implements Initializable {
         }
         return buildingAdress;
     }
+    @FXML
+    private TextField adressInput;
+    @FXML
+    private TextField countryInput;
+    @FXML
+    private TextField cityInput;
+    @FXML
+    private TextField zipInput;
+    @FXML
+    private Label buildingIDT;
+    @FXML
+    private Label roominfo;
+
+    public void addBuildingButton(){
+        String adress, country, city, zip;
+        adress = adressInput.getText();
+        country = countryInput.getText();
+        city = cityInput.getText();
+        zip = zipInput.getText();
+        if(buildingExists(adress, country, city, zip) == false){
+            int buildingID = (int)Math.floor(Math.random()*(99999-10000+1)+10000);
+            String buildingIDString = ""+buildingID;
+            Building newBuilding = new Building(buildingIDString, country, city, adress, zip);
+            DBBuilding.addBuildingToDatabase(newBuilding);
+            roominfo.setText("Building successfully added!");
+            buildingIDT.setText("The buildingID is " + buildingIDString + ", remember this well!");
+        }
+        else{
+            roominfo.setText("The database already contains this building!");
+        }
+    }
+
+    public boolean buildingExists(String adress, String country, String city, String zip){
+        for(Building b : program.getBuildings()){
+            if((b.getAdress().equals(adress)) && (b.getCountry().equals(country)) && (b.getCity().equals(city)) && (b.getZip().equals(zip))){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
