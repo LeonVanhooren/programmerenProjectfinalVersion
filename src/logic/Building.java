a69191a95e5a978c90777abb15e59a1d538bc591
MilanVissers <milan.vissers@ugent.be>
2021-12-08 15:49:08 +0100

    files

--- src/logic/Building.java
+++ src/logic/Building.java
@@ -1,8 +1,11 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Building {
 
     private String buildingID, country, city, adress,zip;
+    private ArrayList<Room> rooms;
 
     public Building(String buildingID, String country, String city, String adress,String zip) {
         this.buildingID = buildingID;
@@ -22,6 +25,10 @@ public class Building {
 
     public String getCountry() {
         return country;
+    }
+
+    public ArrayList<Room> getRooms() {
+        return rooms;
     }
 
     public void setCountry(String country) {
