43cef808aa1a489f4e6ff1b754e156b937bbd7b3
MilanVissers <milan.vissers@ugent.be>
2021-12-13 01:17:11 +0100

    latest version

--- src/database/DBOwnership.java
+++ src/database/DBOwnership.java
@@ -35,6 +35,7 @@ public class DBOwnership {
             e.printStackTrace();
         }
         return ownerships;
+
     }
     public static void addOwnershipToDatabase(Ownership ownership){
         try {
